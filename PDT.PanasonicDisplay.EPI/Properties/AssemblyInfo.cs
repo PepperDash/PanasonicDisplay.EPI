7aec702bd2b43cf9d93636f7b3ed020cbac1992c
Neil Dorin <ndorin@pepperdash.com>
2020-07-22 15:16:12 -0600

    Adds Build actions

--- PDT.PanasonicDisplay.EPI/Properties/AssemblyInfo.cs
+++ PDT.PanasonicDisplay.EPI/Properties/AssemblyInfo.cs
@@ -1,8 +1,11 @@
 ﻿using System.Reflection;
+using Crestron.SimplSharp.Reflection;
 
-[assembly: AssemblyTitle("EssentialsPluginTemplateEpi")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EssentialsPluginTemplateEpi")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: System.Reflection.AssemblyTitle("Panasonic Display Plugin")]
+[assembly: System.Reflection.AssemblyCompany("PepperDash Technology Corp")]
+[assembly: System.Reflection.AssemblyProduct("Panasonic Display Plugin")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright ©  2019")]
+[assembly: System.Reflection.AssemblyVersion("1.0.1.*")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.1.")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("1.0.1.")]
 
